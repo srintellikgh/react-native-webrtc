srintellikgh/react-native-webrtc

commit bea64d5b8e532e6cb43827c801eaa2113482ede5

Promise support (#246)

ios/RCTWebRTC/WebRTCModule+RTCPeerConnection.m
@@ -126,7 +126,8 @@ RCT_EXPORT_METHOD(peerConnectionRemoveStream:(nonnull NSString *)streamID object
 
 RCT_EXPORT_METHOD(peerConnectionCreateOffer:(nonnull NSNumber *)objectID
                                 constraints:(NSDictionary *)constraints
-                                   callback:(RCTResponseSenderBlock)callback)
+                                   resolver:(RCTPromiseResolveBlock)resolve
+                                   rejecter:(RCTPromiseRejectBlock)reject)
 {
   RTCPeerConnection *peerConnection = self.peerConnections[objectID];
   if (!peerConnection) {
@@ -137,22 +138,18 @@ RCT_EXPORT_METHOD(peerConnectionCreateOffer:(nonnull NSNumber *)objectID
     offerForConstraints:[self parseMediaConstraints:constraints]
       completionHandler:^(RTCSessionDescription *sdp, NSError *error) {
         if (error) {
-          callback(@[
-            @(NO),
-            @{
-              @"type": @"CreateOfferFailed",
-              @"message": error.userInfo[@"error"]}
-          ]);
+          reject(@"CreateOfferFailed", error.userInfo[@"error"], error);
         } else {
           NSString *type = [RTCSessionDescription stringForType:sdp.type];
-          callback(@[@(YES), @{@"sdp": sdp.sdp, @"type": type}]);
+          resolve(@{@"sdp": sdp.sdp, @"type": type});
         }
       }];
 }
 
 RCT_EXPORT_METHOD(peerConnectionCreateAnswer:(nonnull NSNumber *)objectID
                                  constraints:(NSDictionary *)constraints
-                                    callback:(RCTResponseSenderBlock)callback)
+                                    resolver:(RCTPromiseResolveBlock)resolve
+                                    rejecter:(RCTPromiseRejectBlock)reject)
 {
   RTCPeerConnection *peerConnection = self.peerConnections[objectID];
   if (!peerConnection) {
@@ -161,22 +158,17 @@ RCT_EXPORT_METHOD(peerConnectionCreateAnswer:(nonnull NSNumber *)objectID
 
   [peerConnection
     answerForConstraints:[self parseMediaConstraints:constraints]
-       completionHandler:^(RTCSessionDescription *sdp, NSError *error) {
-         if (error) {
-           callback(@[
-             @(NO),
-             @{
-               @"type": @"CreateAnswerFailed",
-               @"message": error.userInfo[@"error"]}
-           ]);
-         } else {
-           NSString *type = [RTCSessionDescription stringForType:sdp.type];
-           callback(@[@(YES), @{@"sdp": sdp.sdp, @"type": type}]);
-         }
-       }];
+      completionHandler:^(RTCSessionDescription *sdp, NSError *error) {
+        if (error) {
+          reject(@"CreateAnswerFailed", error.userInfo[@"error"], error);
+        } else {
+          NSString *type = [RTCSessionDescription stringForType:sdp.type];
+          resolve(@{@"sdp": sdp.sdp, @"type": type});
+        }
+      }];
 }
 
-RCT_EXPORT_METHOD(peerConnectionSetLocalDescription:(RTCSessionDescription *)sdp objectID:(nonnull NSNumber *)objectID callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(peerConnectionSetLocalDescription:(RTCSessionDescription *)sdp objectID:(nonnull NSNumber *)objectID resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
   RTCPeerConnection *peerConnection = self.peerConnections[objectID];
   if (!peerConnection) {
@@ -185,16 +177,14 @@ RCT_EXPORT_METHOD(peerConnectionSetLocalDescription:(RTCSessionDescription *)sdp
 
   [peerConnection setLocalDescription:sdp completionHandler: ^(NSError *error) {
     if (error) {
-      id errorResponse = @{@"name": @"SetLocalDescriptionFailed",
-                           @"message": error.localizedDescription};
-      callback(@[@(NO), errorResponse]);
+      reject(@"SetLocalDescriptionFailed", error.localizedDescription, error);
     } else {
-      callback(@[@(YES)]);
+      resolve(nil);
     }
   }];
 }
 
-RCT_EXPORT_METHOD(peerConnectionSetRemoteDescription:(RTCSessionDescription *)sdp objectID:(nonnull NSNumber *)objectID callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(peerConnectionSetRemoteDescription:(RTCSessionDescription *)sdp objectID:(nonnull NSNumber *)objectID resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
   RTCPeerConnection *peerConnection = self.peerConnections[objectID];
   if (!peerConnection) {
@@ -203,16 +193,14 @@ RCT_EXPORT_METHOD(peerConnectionSetRemoteDescription:(RTCSessionDescription *)sd
 
   [peerConnection setRemoteDescription: sdp completionHandler: ^(NSError *error) {
     if (error) {
-      id errorResponse = @{@"name": @"SetRemoteDescriptionFailed",
-                           @"message": error.localizedDescription};
-      callback(@[@(NO), errorResponse]);
+      reject(@"SetRemoteDescriptionFailed", error.localizedDescription, error);
     } else {
-      callback(@[@(YES)]);
+      resolve(nil);
     }
   }];
 }
 
-RCT_EXPORT_METHOD(peerConnectionAddICECandidate:(RTCIceCandidate*)candidate objectID:(nonnull NSNumber *)objectID callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(peerConnectionAddICECandidate:(RTCIceCandidate*)candidate objectID:(nonnull NSNumber *)objectID resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
   RTCPeerConnection *peerConnection = self.peerConnections[objectID];
   if (!peerConnection) {
@@ -221,7 +209,7 @@ RCT_EXPORT_METHOD(peerConnectionAddICECandidate:(RTCIceCandidate*)candidate obje
 
   [peerConnection addIceCandidate:candidate];
   NSLog(@"addICECandidateresult: %@", candidate);
-  callback(@[@true]);
+  resolve(nil);
 }
 
 RCT_EXPORT_METHOD(peerConnectionClose:(nonnull NSNumber *)objectID)
@@ -251,7 +239,8 @@ RCT_EXPORT_METHOD(peerConnectionClose:(nonnull NSNumber *)objectID)
 
 RCT_EXPORT_METHOD(peerConnectionGetStats:(nonnull NSString *)trackID
                                 objectID:(nonnull NSNumber *)objectID
-                                callback:(RCTResponseSenderBlock)callback)
+                                resolver:(RCTPromiseResolveBlock)resolve
+                                rejecter:(RCTPromiseRejectBlock)reject)
 {
   RTCPeerConnection *peerConnection = self.peerConnections[objectID];
   if (!peerConnection) {

MediaStreamTrack.js
@@ -27,7 +27,11 @@ type SourceInfo = {
 
 export default class MediaStreamTrack extends EventTarget(MEDIA_STREAM_TRACK_EVENTS) {
   static getSources(success: (sources: Array<SourceInfo>) => void) {
-    WebRTCModule.mediaStreamTrackGetSources(success);
+    const promise = WebRTCModule.mediaStreamTrackGetSources();
+    if (success) {
+      return promise.then(success);
+    }
+    return promise;
   }
 
   _enabled: boolean;

RTCPeerConnection.js
@@ -12,6 +12,7 @@ import RTCSessionDescription from './RTCSessionDescription';
 import RTCIceCandidate from './RTCIceCandidate';
 import RTCIceCandidateEvent from './RTCIceCandidateEvent';
 import RTCEvent from './RTCEvent';
+import withLegacyCallbacks from './withLegacyCallbacks';
 
 const {WebRTCModule} = NativeModules;
 
@@ -112,6 +113,13 @@ export default class RTCPeerConnection extends EventTarget(PEER_CONNECTION_EVENT
         DEFAULT_PC_CONSTRAINTS,
         this._peerConnectionId);
     this._registerEvents();
+    // Allow for legacy callback usage
+    this.createOffer = withLegacyCallbacks(this.createOffer.bind(this), true);
+    this.createAnswer = withLegacyCallbacks(this.createAnswer.bind(this), true);
+    this.setLocalDescription = withLegacyCallbacks(this.setLocalDescription.bind(this), false, 1, 2);
+    this.setRemoteDescription = withLegacyCallbacks(this.setRemoteDescription.bind(this));
+    this.addIceCandidate = withLegacyCallbacks(this.addIceCandidate.bind(this));
+    this.getStats = withLegacyCallbacks(this.getStats.bind(this));
   }
 
   addStream(stream: MediaStream) {
@@ -142,93 +150,73 @@ export default class RTCPeerConnection extends EventTarget(PEER_CONNECTION_EVENT
     return constraints;
   }
 
-  createOffer(successCallback: ?Function, failureCallback: ?Function, options) {
-    WebRTCModule.peerConnectionCreateOffer(
-        this._peerConnectionId,
-        this._mergeMediaConstraints(options),
-        (successful, data) => {
-          if (successful) {
-            successCallback(new RTCSessionDescription(data));
-          } else {
-            failureCallback(data); // TODO: convert to NavigatorUserMediaError
-          }
-        });
+  createOffer(options) {
+    return WebRTCModule.peerConnectionCreateOffer(
+      this._peerConnectionId,
+      this._mergeMediaConstraints(options)
+    ).then(data => new RTCSessionDescription(data));
   }
 
-  createAnswer(successCallback: ?Function, failureCallback: ?Function, options) {
-    WebRTCModule.peerConnectionCreateAnswer(
-        this._peerConnectionId,
-        this._mergeMediaConstraints(options),
-        (successful, data) => {
-          if (successful) {
-            successCallback(new RTCSessionDescription(data));
-          } else {
-            failureCallback(data);
-          }
-        });
+  createAnswer(options) {
+    return WebRTCModule.peerConnectionCreateAnswer(
+      this._peerConnectionId,
+      this._mergeMediaConstraints(options)
+    ).then(data => new RTCSessionDescription(data));
   }
 
   setConfiguration(configuration) {
     WebRTCModule.peerConnectionSetConfiguration(configuration, this._peerConnectionId);
   }
 
-  setLocalDescription(sessionDescription: RTCSessionDescription, success: ?Function, failure: ?Function, constraints) {
-    WebRTCModule.peerConnectionSetLocalDescription(sessionDescription.toJSON(), this._peerConnectionId, (successful, data) => {
-      if (successful) {
-        this.localDescription = sessionDescription;
-        success();
-      } else {
-        failure(data);
-      }
+  setLocalDescription(sessionDescription: RTCSessionDescription, constraints) {
+    return WebRTCModule.peerConnectionSetLocalDescription(
+      sessionDescription.toJSON(),
+      this._peerConnectionId
+    ).then(() => {
+      this.localDescription = sessionDescription;
+      return;
     });
   }
 
-  setRemoteDescription(sessionDescription: RTCSessionDescription, success: ?Function, failure: ?Function) {
-    WebRTCModule.peerConnectionSetRemoteDescription(sessionDescription.toJSON(), this._peerConnectionId, (successful, data) => {
-      if (successful) {
-        this.remoteDescription = sessionDescription;
-        success();
-      } else {
-        failure(data);
-      }
+  setRemoteDescription(sessionDescription: RTCSessionDescription) {
+    return WebRTCModule.peerConnectionSetRemoteDescription(
+      sessionDescription.toJSON(),
+      this._peerConnectionId
+    ).then(() => {
+      this.remoteDescription = sessionDescription;
+      return;
     });
   }
 
-  addIceCandidate(candidate, success, failure) { // TODO: success, failure
-    WebRTCModule.peerConnectionAddICECandidate(candidate.toJSON(), this._peerConnectionId, (successful) => {
-      if (successful) {
-        success && success();
-      } else {
-        failure && failure();
-      }
-    });
+  addIceCandidate(candidate) { // TODO: success, failure
+    return WebRTCModule.peerConnectionAddICECandidate(
+      candidate.toJSON(),
+      this._peerConnectionId
+    );
   }
 
-  getStats(track, success, failure) {
+  getStats(track) {
     if (WebRTCModule.peerConnectionGetStats) {
-      WebRTCModule.peerConnectionGetStats(
+      return WebRTCModule.peerConnectionGetStats(
         (track && track.id) || '',
-        this._peerConnectionId,
-        stats => {
-          if (success) {
-            // On both Android and iOS it is faster to construct a single
-            // JSON string representing the array of StatsReports and have it
-            // pass through the React Native bridge rather than the array of
-            // StatsReports. While the implementations do try to be faster in
-            // general, the stress is on being faster to pass through the React
-            // Native bridge which is a bottleneck that tends to be visible in
-            // the UI when there is congestion involving UI-related passing.
-            if (typeof stats === 'string') {
-              try {
-                stats = JSON.parse(stats);
-              } catch (e) {
-                failure(e);
-                return;
-              }
-            }
-            success(stats);
+        this._peerConnectionId
+      ).then(stats => {
+        // On both Android and iOS it is faster to construct a single
+        // JSON string representing the array of StatsReports and have it
+        // pass through the React Native bridge rather than the array of
+        // StatsReports. While the implementations do try to be faster in
+        // general, the stress is on being faster to pass through the React
+        // Native bridge which is a bottleneck that tends to be visible in
+        // the UI when there is congestion involving UI-related passing.
+        if (typeof stats === 'string') {
+          try {
+            stats = JSON.parse(stats);
+          } catch (e) {
+            throw e;
           }
-        });
+        }
+        return stats;
+      });
     } else {
       console.warn('RTCPeerConnection getStats not supported');
     }

android/src/main/java/com/oney/WebRTCModule/GetUserMediaImpl.java
@@ -13,6 +13,7 @@ import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableType;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Promise;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -194,15 +195,14 @@ class GetUserMediaImpl {
      */
     void getUserMedia(
             final ReadableMap constraints,
-            final Callback successCallback,
-            final Callback errorCallback,
+            final Promise promise,
             final MediaStream mediaStream) {
         // TODO: change getUserMedia constraints format to support new syntax
         //   constraint format seems changed, and there is no mandatory any more.
         //   and has a new syntax/attrs to specify resolution
         //   should change `parseConstraints()` according
         //   see: https://www.w3.org/TR/mediacapture-streams/#idl-def-MediaTrackConstraints
- 
+
         final ArrayList<String> requestPermissions = new ArrayList<>();
 
         if (constraints.hasKey("audio")) {
@@ -241,7 +241,7 @@ class GetUserMediaImpl {
         // requestedMediaTypes is the empty set, the method invocation fails
         // with a TypeError.
         if (requestPermissions.isEmpty()) {
-            errorCallback.invoke(
+            promise.reject(
                 "TypeError",
                 "constraints requests no media types");
             return;
@@ -256,8 +256,7 @@ class GetUserMediaImpl {
 
                     getUserMedia(
                         constraints,
-                        successCallback,
-                        errorCallback,
+                        promise,
                         mediaStream,
                         grantedPermissions);
                 }
@@ -269,9 +268,10 @@ class GetUserMediaImpl {
                     // getUserMedia() algorithm, if the user has denied
                     // permission, fail "with a new DOMException object whose
                     // name attribute has the value NotAllowedError."
-                    errorCallback.invoke("DOMException", "NotAllowedError");
+                    promise.reject("DOMException", "NotAllowedError");
                 }
-            });
+            }
+        );
     }
 
     /**
@@ -281,8 +281,7 @@ class GetUserMediaImpl {
      */
     private void getUserMedia(
             ReadableMap constraints,
-            Callback successCallback,
-            Callback errorCallback,
+            Promise promise,
             MediaStream mediaStream,
             List<String> grantedPermissions) {
         MediaStreamTrack[] tracks = new MediaStreamTrack[2];
@@ -302,13 +301,14 @@ class GetUserMediaImpl {
              // specified by
              // https://www.w3.org/TR/mediacapture-streams/#dom-mediadevices-getusermedia
              // with respect to distinguishing the various causes of failure.
-             errorCallback.invoke(
+             promise.reject(
                  /* type */ null,
                  "Failed to create new track");
              return;
         }
 
         WritableArray tracks_ = Arguments.createArray();
+        WritableArray successResult = Arguments.createArray();
 
         for (MediaStreamTrack track : tracks) {
             if (track == null) {
@@ -326,7 +326,7 @@ class GetUserMediaImpl {
 
             WritableMap track_ = Arguments.createMap();
             String kind = track.kind();
- 
+
             track_.putBoolean("enabled", track.enabled());
             track_.putString("id", id);
             track_.putString("kind", kind);
@@ -341,7 +341,9 @@ class GetUserMediaImpl {
         Log.d(TAG, "MediaStream id: " + streamId);
         webRTCModule.localStreams.put(streamId, mediaStream);
 
-        successCallback.invoke(streamId, tracks_);
+        successResult.pushString(streamId);
+        successResult.pushArray(tracks_);
+        promise.resolve(successResult);
     }
 
     private VideoTrack getUserVideo(ReadableMap constraints) {
@@ -403,7 +405,7 @@ class GetUserMediaImpl {
                 : DEFAULT_FPS;
 
         videoCapturer.startCapture(width, height, fps);
-  
+
         String trackId = webRTCModule.getNextTrackUUID();
         mVideoCapturers.put(trackId, videoCapturer);
 

android/src/main/java/com/oney/WebRTCModule/PeerConnectionObserver.java
@@ -14,6 +14,7 @@ import android.util.SparseArray;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
@@ -144,7 +145,7 @@ class PeerConnectionObserver implements PeerConnection.Observer {
         }
     }
 
-    void getStats(String trackId, final Callback cb) {
+    void getStats(String trackId, final Promise promise) {
         MediaStreamTrack track = null;
         if (trackId == null
                 || trackId.isEmpty()
@@ -154,7 +155,7 @@ class PeerConnectionObserver implements PeerConnection.Observer {
                     new StatsObserver() {
                         @Override
                         public void onComplete(StatsReport[] reports) {
-                            cb.invoke(statsToJSON(reports));
+                            promise.resolve(statsToJSON(reports));
                         }
                     },
                     track);

android/src/main/java/com/oney/WebRTCModule/WebRTCModule.java
@@ -18,6 +18,7 @@ import com.facebook.react.bridge.ReadableType;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.facebook.react.bridge.Promise;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -34,6 +35,13 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
     private static final String LANGUAGE =  "language";
 
     final PeerConnectionFactory mFactory;
+
+    // Error codes
+    private static final String WEBRTC_CREATE_OFFER_ERROR = "WEBRTC_CREATE_OFFER_ERROR";
+    private static final String WEBRTC_CREATE_ANSWER_ERROR = "WEBRTC_CREATE_ANSWER_ERROR";
+    private static final String WEBRTC_SET_LOCAL_DESCRIPTION_ERROR = "WEBRTC_SET_LOCAL_DESCRIPTION_ERROR";
+    private static final String WEBRTC_SET_REMOTE_DESCRIPTION_ERROR = "WEBRTC_SET_REMOTE_DESCRIPTION_ERROR";
+
     private final SparseArray<PeerConnectionObserver> mPeerConnectionObservers;
     final Map<String, MediaStream> localStreams;
     final Map<String, MediaStreamTrack> localTracks;
@@ -467,9 +475,7 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void getUserMedia(ReadableMap constraints,
-                             Callback    successCallback,
-                             Callback    errorCallback) {
+    public void getUserMedia(ReadableMap constraints, Promise promise) {
         String streamId = getNextStreamUUID();
         MediaStream mediaStream = mFactory.createLocalMediaStream(streamId);
 
@@ -478,19 +484,17 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
             // specified by
             // https://www.w3.org/TR/mediacapture-streams/#dom-mediadevices-getusermedia
             // with respect to distinguishing the various causes of failure.
-            errorCallback.invoke(
+            promise.reject(
                 /* type */ null,
                 "Failed to create new media stream");
             return;
         }
 
-        getUserMediaImpl.getUserMedia(
-            constraints, successCallback, errorCallback,
-            mediaStream);
+        getUserMediaImpl.getUserMedia(constraints, promise, mediaStream);
     }
 
     @ReactMethod
-    public void mediaStreamTrackGetSources(Callback callback){
+    public void mediaStreamTrackGetSources(Promise promise){
         WritableArray array = Arguments.createArray();
         String[] names = new String[Camera.getNumberOfCameras()];
 
@@ -508,7 +512,7 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
         audio.putString("kind", "audio");
 
         array.pushMap(audio);
-        callback.invoke(array);
+        promise.resolve(array);
     }
 
     @ReactMethod
@@ -644,14 +648,14 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
     public void peerConnectionCreateOffer(
             int id,
             ReadableMap constraints,
-            final Callback callback) {
+            final Promise promise) {
         PeerConnection peerConnection = getPeerConnection(id);
 
         if (peerConnection != null) {
             peerConnection.createOffer(new SdpObserver() {
                 @Override
                 public void onCreateFailure(String s) {
-                    callback.invoke(false, s);
+                    promise.reject(WEBRTC_CREATE_OFFER_ERROR, s);
                 }
 
                 @Override
@@ -659,7 +663,7 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
                     WritableMap params = Arguments.createMap();
                     params.putString("sdp", sdp.description);
                     params.putString("type", sdp.type.canonicalForm());
-                    callback.invoke(true, params);
+                    promise.resolve(params);
                 }
 
                 @Override
@@ -670,7 +674,7 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
             }, parseMediaConstraints(constraints));
         } else {
             Log.d(TAG, "peerConnectionCreateOffer() peerConnection is null");
-            callback.invoke(false, "peerConnection is null");
+            promise.reject(WEBRTC_CREATE_OFFER_ERROR, "peerConnection is null");
         }
     }
 
@@ -678,14 +682,14 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
     public void peerConnectionCreateAnswer(
             int id,
             ReadableMap constraints,
-            final Callback callback) {
+            final Promise promise) {
         PeerConnection peerConnection = getPeerConnection(id);
 
         if (peerConnection != null) {
             peerConnection.createAnswer(new SdpObserver() {
                 @Override
                 public void onCreateFailure(String s) {
-                    callback.invoke(false, s);
+                    promise.reject(WEBRTC_CREATE_ANSWER_ERROR, s);
                 }
 
                 @Override
@@ -693,7 +697,7 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
                     WritableMap params = Arguments.createMap();
                     params.putString("sdp", sdp.description);
                     params.putString("type", sdp.type.canonicalForm());
-                    callback.invoke(true, params);
+                    promise.resolve(params);
                 }
 
                 @Override
@@ -704,12 +708,12 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
             }, parseMediaConstraints(constraints));
         } else {
             Log.d(TAG, "peerConnectionCreateAnswer() peerConnection is null");
-            callback.invoke(false, "peerConnection is null");
+            promise.reject(WEBRTC_CREATE_ANSWER_ERROR, "peerConnection is null");
         }
     }
 
     @ReactMethod
-    public void peerConnectionSetLocalDescription(ReadableMap sdpMap, final int id, final Callback callback) {
+    public void peerConnectionSetLocalDescription(ReadableMap sdpMap, final int id, final Promise promise) {
         PeerConnection peerConnection = getPeerConnection(id);
 
         Log.d(TAG, "peerConnectionSetLocalDescription() start");
@@ -726,7 +730,7 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
 
                 @Override
                 public void onSetSuccess() {
-                    callback.invoke(true);
+                    promise.resolve(null);
                 }
 
                 @Override
@@ -735,17 +739,17 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
 
                 @Override
                 public void onSetFailure(String s) {
-                    callback.invoke(false, s);
+                    promise.reject(WEBRTC_SET_LOCAL_DESCRIPTION_ERROR, s);
                 }
             }, sdp);
         } else {
             Log.d(TAG, "peerConnectionSetLocalDescription() peerConnection is null");
-            callback.invoke(false, "peerConnection is null");
+            promise.reject(WEBRTC_SET_LOCAL_DESCRIPTION_ERROR, "peerConnection is null");
         }
         Log.d(TAG, "peerConnectionSetLocalDescription() end");
     }
     @ReactMethod
-    public void peerConnectionSetRemoteDescription(final ReadableMap sdpMap, final int id, final Callback callback) {
+    public void peerConnectionSetRemoteDescription(final ReadableMap sdpMap, final int id, final Promise promise) {
         PeerConnection peerConnection = getPeerConnection(id);
         // final String d = sdpMap.getString("type");
 
@@ -763,7 +767,7 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
 
                 @Override
                 public void onSetSuccess() {
-                    callback.invoke(true);
+                    promise.resolve(null);
                 }
 
                 @Override
@@ -772,17 +776,17 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
 
                 @Override
                 public void onSetFailure(String s) {
-                    callback.invoke(false, s);
+                    promise.reject(WEBRTC_SET_REMOTE_DESCRIPTION_ERROR, s);
                 }
             }, sdp);
         } else {
             Log.d(TAG, "peerConnectionSetRemoteDescription() peerConnection is null");
-            callback.invoke(false, "peerConnection is null");
+            promise.reject(WEBRTC_SET_REMOTE_DESCRIPTION_ERROR, "peerConnection is null");
         }
         Log.d(TAG, "peerConnectionSetRemoteDescription() end");
     }
     @ReactMethod
-    public void peerConnectionAddICECandidate(ReadableMap candidateMap, final int id, final Callback callback) {
+    public void peerConnectionAddICECandidate(ReadableMap candidateMap, final int id, final Promise promise) {
         boolean result = false;
         PeerConnection peerConnection = getPeerConnection(id);
         Log.d(TAG, "peerConnectionAddICECandidate() start");
@@ -796,17 +800,17 @@ public class WebRTCModule extends ReactContextBaseJavaModule {
         } else {
             Log.d(TAG, "peerConnectionAddICECandidate() peerConnection is null");
         }
-        callback.invoke(result);
+        promise.resolve(result);
         Log.d(TAG, "peerConnectionAddICECandidate() end");
     }
 
     @ReactMethod
-    public void peerConnectionGetStats(String trackId, int id, Callback cb) {
+    public void peerConnectionGetStats(String trackId, int id, final Promise promise) {
         PeerConnectionObserver pco = mPeerConnectionObservers.get(id);
         if (pco == null || pco.getPeerConnection() == null) {
             Log.d(TAG, "peerConnectionGetStats() peerConnection is null");
         } else {
-            pco.getStats(trackId, cb);
+            pco.getStats(trackId, promise);
         }
     }
 

commit 28bed204244282d9263db3c5c4b1911c849f2b6d

ios: fix build
Build was broken after #246 landed.

ios/RCTWebRTC/WebRTCModule+RTCPeerConnection.m
@@ -255,7 +255,7 @@ RCT_EXPORT_METHOD(peerConnectionGetStats:(nonnull NSString *)trackID
     [peerConnection statsForTrack:track
                  statsOutputLevel:RTCStatsOutputLevelStandard
                 completionHandler:^(NSArray<RTCLegacyStatsReport *> *stats) {
-                  callback(@[[self statsToJSON:stats]]);
+                  resolve(@[[self statsToJSON:stats]]);
                 }];
   }
 }